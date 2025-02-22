bcc7e9917632b1b625130bb60d2efa9db7f9e1dc
Mariusz Kryński <mrk@sed.pl>
2025-02-22 23:57:02 +0100

    move esp_log declaration to co_core.h

--- src/core/co_core.h
+++ src/core/co_core.h
@@ -206,6 +206,8 @@ void CONodeProcess(CO_NODE *node);
 */
 extern void CONodeFatalError(void);
 
+extern void esp_log(const char *tag, const char *fmt, ...);
+
 #ifdef __cplusplus               /* for compatibility with C++ environments  */
 }
 #endif
